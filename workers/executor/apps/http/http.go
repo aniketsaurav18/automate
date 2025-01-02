f5f9b8d3607722c22e0cb48eb3c54e79449dc0db
Saurav Sharma <aniketsaurav18@gmail.com>
2025-01-02 12:19:04 +0000

    completed post request and added get request

--- workers/executor/apps/http/http.go
+++ workers/executor/apps/http/http.go
@@ -2,35 +2,145 @@ package http
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func PostRequestHandler (h *HttpJob) {
+var HttpClient = &http.Client{}
+
+func PostRequestHandler(h *HttpJob) {
 	post_body := []byte(h.Input.Body)
 	req_url := h.Input.URL
 
-	if len(h.Input.Parameters) != 0 {
-		req_url += "?"
-		for key,val := range h.Input.Parameters{
-			req_url += key+"="+val
-		}
-	} 
-	
-	req, err := http.NewRequest(h.Input.Method, h.Input.URL, bytes.NewBuffer(post_body))
-	if(err != nil){
-		fmt.Println("Error while making post req")
+	// Create a new HTTP request
+	req, err := http.NewRequest(h.Input.Method, req_url, bytes.NewBuffer(post_body))
+	if err != nil {
+		fmt.Println("Error while making post request")
 		return
 	}
 
-	for key, val:= range h.Input.Headers {
-		req.Header.Set(key, val);
+	// Add parameters to the URL if any
+	if len(h.Input.Parameters) != 0 {
+		q := req.URL.Query()
+		for key, val := range h.Input.Parameters {
+			q.Add(key, val)
+		}
+		req.URL.RawQuery = q.Encode()
 	}
 
-	
+	// Set headers
+	if len(h.Input.Headers) != 0 {
+		for key, val := range h.Input.Headers {
+			req.Header.Set(key, val)
+		}
+	}
 
+	// Create an HTTP client and execute the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	var result = &HttpJobOutput{}
+	if err != nil {
+		fmt.Println("Error while making request")
+		fmt.Println(err)
+	} else {
+		result.StatusCode = res.StatusCode
+		result.Headers = res.Header
+
+		// Read the response body
+		res_body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("Error while reading the body")
+			result.Body = "Error while reading the body"
+		} else {
+			result.Body = string(res_body)
+		}
+	}
+
+	fmt.Println("Response Status Code")
+	fmt.Println(result.StatusCode)
+	fmt.Println("Response Headers")
+	err = nil
+	formatted_headers, err := json.MarshalIndent(result.Headers, "", "  ")
+	if err != nil {
+		fmt.Println("error while marshaling the response headers")
+	}
+	fmt.Println(string(formatted_headers))
+	fmt.Println("Response Body:")
+	fmt.Println(result.Body)
 }
 
-func ExecuteHttpJob (h *HttpJob) {
-	http.NewRequest(h.Key, h.Input.URL, )
+func GetRequestHandler(h *HttpJob) {
+	if h.Key != "http" || h.Input.Method != "GET" {
+		return
+	}
+	req_url := h.Input.URL
+
+	// Create a new HTTP request
+	req, err := http.NewRequest(h.Input.Method, req_url, nil)
+	if err != nil {
+		fmt.Println("Error while making get request")
+		return
+	}
+
+	// Add parameters to the URL if any
+	if len(h.Input.Parameters) != 0 {
+		q := req.URL.Query()
+		for key, val := range h.Input.Parameters {
+			q.Add(key, val)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	// Set headers if any
+	if len(h.Input.Headers) != 0 {
+		for key, val := range h.Input.Headers {
+			req.Header.Set(key, val)
+		}
+	}
+
+	res, err := HttpClient.Do(req)
+	if err != nil {
+		fmt.Println("Error while doing get request")
+	}
+
+	var result = &HttpJobOutput{}
+	if err != nil {
+		fmt.Println("Error while making request")
+		fmt.Println(err)
+	} else {
+		result.StatusCode = res.StatusCode
+		result.Headers = res.Header
+
+		// Read the response body
+		res_body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("Error while reading the body")
+			result.Body = "Error while reading the body"
+		} else {
+			result.Body = string(res_body)
+		}
+	}
+	fmt.Println("Response Status Code")
+	fmt.Println(result.StatusCode)
+	fmt.Println("Response Headers")
+	err = nil
+	formatted_headers, err := json.MarshalIndent(result.Headers, "", "  ")
+	if err != nil {
+		fmt.Println("error while marshaling the response headers")
+	}
+	fmt.Println(string(formatted_headers))
+	fmt.Println("Response Body:")
+	fmt.Println(result.Body)
+}
+
+func ExecuteHttpJob(h *HttpJob) {
+	if h.Input.Method == "GET" {
+		GetRequestHandler(h)
+	} else if h.Input.Method == "POST" {
+		PostRequestHandler(h)
+	} else {
+		fmt.Println("Unrecognised method.")
+	}
 }
--- workers/executor/main.go
+++ workers/executor/main.go
@@ -4,11 +4,33 @@ import (
 	"executor/apps/http"
 )
 
-p := HttpJob{
-	Key: 
-}
-
-
 func main() {
-	httpexecutor.ExecuteHttpJob();
+	// job := http.HttpJob{
+	// 	Key: "http",
+	// 	Input: http.HttpJobInput{
+	// 		URL:    "https://jsonplaceholder.typicode.com/posts",
+	// 		Method: "POST",
+	// 		Headers: map[string]string{
+	// 			"Content-Type": "application/json; charset=UTF-8",
+	// 		},
+	// 		// Parameters: map[string]string{
+	// 		// 	"query": "golang",
+	// 		// },
+	// 		Body: `{"title": "foo", "body": "bar", "userId": 1}`,
+	// 	},
+	// }
+	job2 := http.HttpJob{
+		Key: "http",
+		Input: http.HttpJobInput{
+			URL:    "https://jsonplaceholder.typicode.com/comments",
+			Method: "GET",
+			Headers: map[string]string{
+				"Content-Type": "application/json; charset=UTF-8",
+			},
+			Parameters: map[string]string{
+				"postId": "1",
+			},
+		},
+	}
+	http.ExecuteHttpJob(&job2)
 }
